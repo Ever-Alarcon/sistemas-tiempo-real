e774ba294fd4ea4e3bfc980610ee4f40dbd1289a
Ever-Alarcon <juni07alarcon@gmail.com>
2024-03-08 23:28:31 -0500

    Tarea del pulsador

--- 00_1/main/blink_example_main.c
+++ 00_1/main/blink_example_main.c
@@ -1,104 +1,82 @@
-/* Blink Example
-
-   This example code is in the Public Domain (or CC0 licensed, at your option.)
-
-   Unless required by applicable law or agreed to in writing, this
-   software is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR
-   CONDITIONS OF ANY KIND, either express or implied.
-*/
 #include <stdio.h>
 #include "freertos/FreeRTOS.h"
 #include "freertos/task.h"
 #include "driver/gpio.h"
 #include "esp_log.h"
-#include "led_strip.h"
-#include "sdkconfig.h"
 
-static const char *TAG = "example";
+/* GPIO para el LED */
+#define BLINK_GPIO 2
 
-/* Use project configuration menu (idf.py menuconfig) to choose the GPIO to blink,
-   or you can edit the following line and set a number here.
-*/
-#define BLINK_GPIO CONFIG_BLINK_GPIO
+/* GPIO para el botón */
+#define BUTTON_GPIO 18
 
 static uint8_t s_led_state = 0;
-
-#ifdef CONFIG_BLINK_LED_STRIP
-
-static led_strip_handle_t led_strip;
+#define ESP_INTR_FLAG_DEFAULT 0
 
 static void blink_led(void)
 {
-    /* If the addressable LED is enabled */
-    if (s_led_state) {
-        /* Set the LED pixel using RGB from 0 (0%) to 255 (100%) for each color */
-        led_strip_set_pixel(led_strip, 0, 16, 16, 16);
-        /* Refresh the strip to send data */
-        led_strip_refresh(led_strip);
-    } else {
-        /* Set all LED off to clear all pixels */
-        led_strip_clear(led_strip);
-    }
+    /* Encender el LED */
+    gpio_set_level(BLINK_GPIO, 0);
+    vTaskDelay(500 / portTICK_PERIOD_MS);  // Esperar 500 ms
+    /* Apagar el LED */
+    gpio_set_level(BLINK_GPIO, 1);
+    vTaskDelay(500 / portTICK_PERIOD_MS);  // Esperar 500 ms
 }
 
 static void configure_led(void)
 {
-    ESP_LOGI(TAG, "Example configured to blink addressable LED!");
-    /* LED strip initialization with the GPIO and pixels number*/
-    led_strip_config_t strip_config = {
-        .strip_gpio_num = BLINK_GPIO,
-        .max_leds = 1, // at least one LED on board
-    };
-#if CONFIG_BLINK_LED_STRIP_BACKEND_RMT
-    led_strip_rmt_config_t rmt_config = {
-        .resolution_hz = 10 * 1000 * 1000, // 10MHz
-        .flags.with_dma = false,
-    };
-    ESP_ERROR_CHECK(led_strip_new_rmt_device(&strip_config, &rmt_config, &led_strip));
-#elif CONFIG_BLINK_LED_STRIP_BACKEND_SPI
-    led_strip_spi_config_t spi_config = {
-        .spi_bus = SPI2_HOST,
-        .flags.with_dma = true,
-    };
-    ESP_ERROR_CHECK(led_strip_new_spi_device(&strip_config, &spi_config, &led_strip));
-#else
-#error "unsupported LED strip backend"
-#endif
-    /* Set all LED off to clear all pixels */
-    led_strip_clear(led_strip);
-}
-
-#elif CONFIG_BLINK_LED_GPIO
-
-static void blink_led(void)
-{
-    /* Set the GPIO level according to the state (LOW or HIGH)*/
-    gpio_set_level(BLINK_GPIO, s_led_state);
-}
-
-static void configure_led(void)
-{
-    ESP_LOGI(TAG, "Example configured to blink GPIO LED!");
     gpio_reset_pin(BLINK_GPIO);
-    /* Set the GPIO as a push/pull output */
     gpio_set_direction(BLINK_GPIO, GPIO_MODE_OUTPUT);
 }
 
-#else
-#error "unsupported LED type"
-#endif
+static void configure_button(void)
+{
+    gpio_reset_pin(BUTTON_GPIO);
+    gpio_set_direction(BUTTON_GPIO, GPIO_MODE_INPUT);
+    gpio_set_pull_mode(BUTTON_GPIO, GPIO_PULLUP_ONLY);
+}
+
+static void IRAM_ATTR button_isr_handler(void* arg)
+{
+    /* Cambiar el estado del LED cuando se presione el botón */
+    s_led_state = !s_led_state;
+    if (s_led_state) {
+        blink_led();
+    }
+}
 
 void app_main(void)
 {
-
-    /* Configure the peripheral according to the LED type */
+    /* Configurar el LED */
     configure_led();
+    /* Configurar el botón */
+    configure_button();
+    
+    gpio_config_t io_conf = {};
+    io_conf.intr_type = GPIO_INTR_POSEDGE;
+    io_conf.pin_bit_mask = (1ULL << BUTTON_GPIO);
+    io_conf.mode = GPIO_MODE_INPUT;
+    io_conf.pull_up_en = GPIO_PULLUP_ENABLE;
+    
+    gpio_config(&io_conf);
 
+    /* Instalar el servicio de ISR para el botón */
+    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+    /* Asignar el manejador de ISR para el botón */
+    gpio_isr_handler_add(BUTTON_GPIO, button_isr_handler, NULL);
+    
     while (1) {
-        ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
-        blink_led();
-        /* Toggle the LED state */
-        s_led_state = !s_led_state;
-        vTaskDelay(CONFIG_BLINK_PERIOD / portTICK_PERIOD_MS);
+        
+        if(gpio_get_level(BUTTON_GPIO)==0){
+            /* Titilar el LED */
+            blink_led();
+            s_led_state = !s_led_state;
+            /* Retardo para el titilado del LED */
+            /*vTaskDelay(500/ portTICK_PERIOD_MS);*/
+        }
+        else {
+            gpio_set_level(BLINK_GPIO, 0);
+            s_led_state= !s_led_state;
+        }
     }
 }
